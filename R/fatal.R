677e1d905b4c59e90aeb076ccf4a041d93672bc8
Paul Teetor <paulteetor@yahoo.com>
2020-09-28 10:03:05 -0500

    Improve fatal() functions. Add %||% operator.

--- R/fatal.R
+++ R/fatal.R
@@ -1,19 +1,22 @@
 #' @export
-fatal = function(..., sep = " ") {
-  msg = paste(..., sep = sep)
+fatal = function(..., sep = " ", caller = NULL) {
+  caller <- caller %||% as.list(sys.call(-1))[[1]]
+  msg <- paste0("[", caller, "] ", paste(..., sep = sep))
   stop(msg, call. = FALSE)
 }
 
 #' @export
-fatalIf = function(cond, ...) {
+fatalIf = function(cond, ..., caller = NULL) {
   if (cond) {
-    fatal(...)
+    fatal(...,
+          caller = caller %||% as.list(sys.call(-1))[[1]] )
   }
 }
 
 #' @export
-fatalIfNot = function(cond, ...) {
+fatalIfNot = function(cond, ..., caller = NULL) {
   if (!cond) {
-    fatal(...)
+    fatal(...,
+          caller = caller %||% as.list(sys.call(-1))[[1]] )
   }
 }
