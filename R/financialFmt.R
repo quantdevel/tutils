3d6f02f04579a5dc1ecb4539a5c72690199cff63
Paul Teetor <paulteetor@yahoo.com>
2021-05-24 12:10:43 -0500

    Improve financialFmt(), add dollarFmt()

--- R/financialFmt.R
+++ R/financialFmt.R
@@ -1,16 +1,24 @@
 #'
 #'  Format a number in "financial" format
 #'
-#' @param x Value to be formatted
-#' @param digits Round to this number of digitis, default 2
-#'
+#' @param x Value to be formatted (numeric)
+#' @param prefix Prefix to prepend, typically a currency symbol (character, optional)
+#' @param accuracy Accuracy of formatted result;
+#'   e.g., 1 for rounding to nearest dollar, 0.01 for rounding to nearest penny
+#'   (numeric, optional)
+#' @param ... Passed to \code{financialFmr}
 #' @return A string
-#'
 #' @export
 #'
-financialFmt = function(x, digits=2) {
-    x = round(x, digits)
-    s = scales::comma(abs(x))
+financialFmt = function(x, prefix = "", accuracy = 1) {
+    s = scales::comma(abs(x), accuracy = accuracy)
+    s = paste0(prefix, s)
     s = ifelse(x >= 0, s, paste0("(", s, ")"))
     return(s)
+}
+
+#' @rdname financialFmt
+#' @export
+dollarFmt = function(x, ...) {
+  financialFmt(x, prefix = "$", ...)
 }
