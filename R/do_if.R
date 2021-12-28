6c98c3d516e2bc61757f1ac37b8443cc8a405847
Paul Teetor <paulteetor@yahoo.com>
2021-12-28 17:26:21 -0600

    Add do_if() function

---
+++ R/do_if.R
@@ -0,0 +1,22 @@
+#'
+#' Conditionally apply expressions to a data object
+#' 
+#' @param .data Input data
+#' @param condition A logical value to determine whether to use .then or .else
+#' @param .then Formula or function to apply to input data when condition is TRUE
+#' @param .else Formula or function to apply to input data when condition is FALSE;
+#'   if NULL and condition is FALSE, data is left unchanged
+#' @return Output of appropriate .then/.else call
+#' @export
+#' @importFrom rlang as_closure
+#'
+do_if <- function(.data, condition, .then, .else = NULL) {
+  if (condition) {
+    call <- rlang::as_closure(.then)
+  } else if (!is.null(.else)) {
+    call <- rlang::as_closure(.else)
+  } else {
+    return(.data)
+  }
+  do.call(call, list(.data))
+}
