5420526512a069079dd5b3e603b24163aacf0d04
Paul Teetor <paulteetor@yahoo.com>
2022-02-17 12:07:45 -0600

    Define new %or% functional

--- R/operators.R
+++ R/operators.R
@@ -12,6 +12,29 @@
 #' 1 %||% 2
 #' NULL %||% 2
 #'
-`%||%` <- function(x, y) {
+`%||%` = function(x, y) {
   if (is.null(x)) y else x
 }
+
+#'
+#'  Combine two predicates
+#'
+#'  Given two predicates, this operator combines them
+#'  into one, new predicate. The new predicate calls
+#'  the given predicates, applying "||" to the results.
+#'
+#'  Typical usage within the Tau sofware would be
+#'  \code{decl(x, is.null %or% is.numeric)}.
+#'
+#' @param p1 A predicate of one argument
+#' @param p2 A predicate of one argument
+#' @return Returns a \emph{function}, not
+#'   a value. (Be careful.)
+#' @export
+#' @name op-disjunction-functional
+#' @examples
+#' (is.null %or% is.numeric)(NULL)
+#' (is.null %or% is.numeric)(pi)
+#' (is.null %or% is.numeric)("foo")
+#'
+`%or%` = function(p1, p2) function(x) p1(x) || p2(x)
