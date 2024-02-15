842b4d66f30a4634ad208da10bb6dea204928c77
Paul Teetor <paulteetor@yahoo.com>
2024-02-15 08:18:46 -0600

    Doc only change

--- R/fatal.R
+++ R/fatal.R
@@ -1,10 +1,20 @@
+#'
+#' Report or test a fatal condition
+#'
+#' @param ... Passed to \code{cat}
+#' @param cond Boolean expression that must be FALSE (\code{fatalIf})
+#'   or must be TRUE (\code{fatalIfNot}), otherwise a \code{stop} error is signalled
+#' @param sep Separator, passed to \code{cat} (character)
+#' @param caller Name of calling function, defaults to name in caller's stack frame (character)
 #' @export
+#'
 fatal = function(..., sep = " ", caller = NULL) {
   caller <- caller %||% as.list(sys.call(-1))[[1]]
   msg <- paste0("[", caller, "] ", paste(..., sep = sep))
   stop(msg, call. = FALSE)
 }
 
+#' @rdname fatal
 #' @export
 fatalIf = function(cond, ..., caller = NULL) {
   if (cond) {
@@ -13,6 +23,7 @@ fatalIf = function(cond, ..., caller = NULL) {
   }
 }
 
+#' @rdname fatal
 #' @export
 fatalIfNot = function(cond, ..., caller = NULL) {
   if (!cond) {
