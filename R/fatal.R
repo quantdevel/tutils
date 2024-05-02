8b21472fff932d8c6509bc8435edec53b15b817d
Paul Teetor <paulteetor@yahoo.com>
2024-05-02 16:15:00 -0500

    Make fatal() more robust

--- R/fatal.R
+++ R/fatal.R
@@ -9,8 +9,19 @@
 #' @export
 #'
 fatal = function(..., sep = " ", caller = NULL) {
-  caller <- caller %||% as.list(sys.call(-1))[[1]]
-  msg <- paste0("[", caller, "] ", paste(..., sep = sep))
+  msg <- paste(..., sep = sep)
+
+  caller <- caller %||%
+    {
+      calls <- sys.call(-1)
+      if (!is.null(calls)) {
+        as.list(calls[[1]])
+      }
+    }
+  if (!is.null(caller)) {
+    msg <- paste0("[", caller, "] ", msg)
+  }
+
   stop(msg, call. = FALSE)
 }
 
