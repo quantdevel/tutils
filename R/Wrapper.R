6dfb289e870835f9e82b587816817d1a4b8cf443
Paul Teetor <paulteetor@yahoo.com>
2025-01-18 15:09:42 -0600

    wrap function: Add 'include' parameter

--- R/Wrapper.R
+++ R/Wrapper.R
@@ -16,6 +16,8 @@
 #' @param errors List of error messages (list, optional)
 #' @param warnings List of warning messages (list, optional)
 #' @param info List of informational messages (list, optional)
+#' @param include A list of already-wrapped objects, who's
+#'   errors, warnings, and infos should be included in this wrapper
 #' @param quiet If TRUE, print the enclosed messages, if any, before
 #'   extracting the enclosed value (logical)
 #' @returns \code{Wrapper} and \code{wrap} eturn a list with class \code{Wrapper}.
@@ -30,10 +32,19 @@
 #' # Extract the wrapped value and print the wrapped messages
 #' unwrap(w, quiet = FALSE)
 #'
-Wrapper = function(value, errors = list(), warnings = list(), info = list(), ...) {
+#' # Piggy back the error messages
+#' wrap(pi, error = "Pi error", include = list( wrap("foo", error = "Foo error") ))
+#'
+Wrapper = function(value, errors = list(), warnings = list(), info = list(),
+                   include = list(), ...) {
   errors <- as.list(errors)       # in case 'errors' is.character or NULL
   warnings <- as.list(warnings)   # ditto
   info <- as.list(info)           # ditto
+
+  # Incorporate the other included messages
+  errors <- c(errors, unlist(purrr::map(include, "errors")))
+  warnings <- c(warnings, unlist(purrr::map(include, "warnings")))
+  info <- c(info, unlist(purrr::map(include, "info")))
 
   (append(list(value = value,
                errors = errors,
