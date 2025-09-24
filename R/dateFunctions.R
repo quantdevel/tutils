0cbd008a56ac25e7235399da8351647fe29c3b83
Paul Teetor <paulteetor@yahoo.com>
2025-09-24 09:35:09 -0500

    Add function: ddd_mm_dd()

--- R/dateFunctions.R
+++ R/dateFunctions.R
@@ -14,3 +14,19 @@ yyyymmddToDate = function(yyyymmdd) {
                   (yyyymmdd %/% 100) %% 100,
                   (yyyymmdd %%  100) ))
 }
+
+#'
+#' Format a date concisely
+#'
+#' Format a date as "MMM, DD/MM", for example "Wed, 09/24".
+#'
+#' This format is useful in contexts where space is tight, readability is valuable,
+#' and the year is understood (or not important).
+#'
+#' @param d Date object
+#' @returns Returns the formatted date (character)
+#' @export
+#'
+ddd_mm_dd = function(d) {
+    format(d, "%a, %m/%d")
+}
